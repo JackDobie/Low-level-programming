e4ddb4fddb20f0d2d22556c11a277ec70c712fd4
Jack Dobie <57402187+JackDobie@users.noreply.github.com>
2021-11-11 16:25:56 +0000

    Update Global.cpp

--- RayTracerSmall/Global.cpp
+++ RayTracerSmall/Global.cpp
@@ -11,19 +11,21 @@ void* operator new(size_t size)
 
 void* operator new(size_t size, Heap* pHeap)
 {
+	std::cout << "New" << std::endl;
 	size_t nRequestedBytes = size + sizeof(Header) + sizeof(Footer);
 	char* pMem = (char*)malloc(nRequestedBytes);
 	Header* pHeader = (Header*)pMem;
 
 	pHeader->size = size;
 	pHeader->heap = pHeap;
-	pHeader->heap->AddAllocation(size);
 	pHeader->checkVal = checkValHeader;
 
 	void* pFooterAddr = pMem + sizeof(Header) + size;
 	Footer* pFooter = (Footer*)pFooterAddr;
-
 	pFooter->checkVal = checkValFooter;
+
+	std::cout << pHeader->size << std::endl;
+	pHeader->heap->AddAllocation(pHeader->size + sizeof(pHeader) + sizeof(pFooter));
 
 	void* pStartMemBlock = pMem + sizeof(Header);
 	return pStartMemBlock;
@@ -31,12 +33,15 @@ void* operator new(size_t size, Heap* pHeap)
 
 void operator delete(void* pMem)
 {
+	std::cout << "Delete" << std::endl;
 	Header* pHeader = (Header*)((char*)pMem - sizeof(Header));
 	assert(pHeader->checkVal == checkValHeader);
-	pHeader->heap->RemoveAllocation(pHeader->size);
 
 	Footer* pFooter = (Footer*)((char*)pMem + pHeader->size);
 	assert(pFooter->checkVal == checkValFooter);
+
+	std::cout << pHeader->size << std::endl;
+	pHeader->heap->RemoveAllocation(pHeader->size + sizeof(pHeader) + sizeof(pFooter));
 
 	free(pHeader);
 }
